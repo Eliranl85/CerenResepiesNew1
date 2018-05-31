9ab8ba5c658467d3b80f75c343d5c7e184dda96d
danielmedalsi1234 <danielmedalsi1234@gmail.com>
2018-05-31 22:25:08 +0300

    Initial commit

---
+++ app/src/main/java/il/co/beeart/cerenresepies/MainActivity.java
@@ -0,0 +1,28 @@
+package il.co.beeart.cerenresepies;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+    private Button btnGoToMain;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnGoToMain = findViewById(R.id.btnGoToStart);
+
+        btnGoToMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,MainMenu.class));
+            }
+        });
+    }
+
+
+}
